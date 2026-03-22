822d22151c17baf851f4bb848235c2d58daa7a98
DineshPawar7 <dsp70458@gmail.com>
2026-03-22 12:30:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-blue-900">
+      <div className="p-8 bg-white rounded-xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-4">
+           e-construct.in
+        </h1>
+        <p className="text-gray-700">
+         is under-Development
+        </p>
+       
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
